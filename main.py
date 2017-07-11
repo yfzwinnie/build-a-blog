58d74fa8d5627f64afdcccb59076d8b79fa6c789
Winnie Zhang <yfz.winnie@gmail.com>
2017-07-10 19:58:31 -0700

    Changing database name and password

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 app= Flask(__name__)
 
 app.config['DEBUG']=True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','mysql+pymysql://build-a-blog:pooh@localhost:8889/build-a-blog')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgres://diognhexsqognq:aa18612b2b57045a7b3f58942999b754dc4c2f55db9c6d47691a435505381124@ec2-184-73-199-72.compute-1.amazonaws.com:5432/devo8ico0tbo2l')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
