f1205a4021ad1414da6f7a42497393dddaab78cf
Winnie Zhang <yfz.winnie@gmail.com>
2017-07-10 19:45:51 -0700

    change app config on main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 app= Flask(__name__)
 
 app.config['DEBUG']=True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('mysql+pymysql://build-a-blog:pooh@localhost:8889/build-a-blog')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','mysql+pymysql://build-a-blog:pooh@localhost:8889/build-a-blog')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
