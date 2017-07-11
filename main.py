ebb6923275d583b873a180d104bde5b63ac3da0e
Winnie Zhang <yfz.winnie@gmail.com>
2017-07-10 19:21:58 -0700

    Added Postgresql to Heroku

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from werkzeug import generate_password_hash, check_password_hash
 app= Flask(__name__)
 
 app.config['DEBUG']=True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:pooh@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/build_a_blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
